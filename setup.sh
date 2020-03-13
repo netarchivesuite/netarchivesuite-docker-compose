c157e244c4468a7eff5a2b72a3f9b7c5fb6957e6
csrster <csr@statsbiblioteket.dk>
2020-03-13 08:43:32 +0100

    Added a setup script to distribute the master copy of RepositorySettings.xml to all applications.

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+RS=$DIR/bitmag-conf/service-conf/RepositorySettings.xml
+cp $RS $DIR/bitmag-conf/service-conf/alarmservice
+cp $RS $DIR/bitmag-conf/service-conf/audittrailservice
+cp $RS $DIR/bitmag-conf/service-conf/service
+cp $RS $DIR/bitmag-conf/service-conf/integrityservice
+cp $RS $DIR/bitmag-conf/service-conf/monitoringservice
+cp $RS $DIR/bitmag-conf/service-conf/webclient
+cp $RS $DIR/filepillar
+cp $RS $DIR/nasapp/nasclientconfig
