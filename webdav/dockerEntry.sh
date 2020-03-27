e8294cf0a4666cc692860ab550356f2c1ba3ae24
csrster <csr@statsbiblioteket.dk>
2020-03-27 11:17:36 +0100

    Minor changes to how webdav starts.

--- webdav/dockerEntry.sh
+++ webdav/dockerEntry.sh
@@ -1,25 +1,36 @@
 #!/bin/bash
-set -e
 
-# If "-e uid={custom/local user id}" flag is not set for "docker run" command, use 9999 as default
-CURRENT_UID=${uid:-$(stat -c "%u" /site/dav)}
-CURRENT_GID=${gid:-$(stat -c "%g" /site/dav)}
+echo ""
+echo "$(date) Starting up"
 
-echo "Current UID : $CURRENT_UID, default = owner of /site/dav = $(stat -c "%u" /site/dav)"
-echo "Current UID : $CURRENT_UID, default = owner of /site/dav = $(stat -c "%g" /site/dav)"
+# If apache have not run, assume first startup and fix  the
+if ! (id docker &> /dev/null); then
 
-groupadd --non-unique --gid ${CURRENT_GID} docker #Create it
-# Create user called "docker" with selected UID
-useradd --shell /bin/bash \
-        --uid ${CURRENT_UID} \
-        --gid ${CURRENT_GID} \
-        --non-unique \
-        --comment "" \
-        --create-home \
-        docker   || true
+	echo "User 'docker' not found in container so creating"
 
+	# If "-e uid={custom/local user id}" flag is not set for "docker run" command, use /site/dav     owner as default
+	DOCKER_UID=${uid:-$(stat -c "%u" /site/dav)}
+	DOCKER_GID=${gid:-$(stat -c "%g" /site/dav)}
+
+	echo "Docker UID : $DOCKER_UID, default = owner of /site/dav = $(stat -c "%u" /site/dav)"
+	echo "Docker UID : $DOCKER_UID, default = owner of /site/dav = $(stat -c "%g" /site/dav)"
+
+	#Create new docker group
+	groupadd --non-unique --gid ${DOCKER_GID} docker
+
+	# Create user called "docker" with selected UID
+	useradd --shell /bin/bash \
+	        --uid ${DOCKER_UID} \
+	        --gid ${DOCKER_GID} \
+	        --non-unique \
+	        --comment "" \
+	        --create-home \
+	        docker
+fi
+
+rm -f /run/apache2/apache2.pid
 #So find the username and groupname
 echo "Running apache as $(id docker)"
 
 #Execute the command
-exec $*
+exec /usr/sbin/apachectl -D FOREGROUND
