25b2cd3c84099a963a6b3598d958b667993d1813
csrster <csr@statsbiblioteket.dk>
2020-05-14 15:19:06 +0200

    Various tweaks to get clearBitmag.sh to work

--- nasapp/clearBitmag.sh
+++ nasapp/clearBitmag.sh
@@ -10,13 +10,16 @@ PILLARS="file1-pillar"
 listing=$(mktemp)
 trap "rm -f $listing" 0 2 3 5
 
-java -cp "/nas/lib/*" org.bitrepository.commandline.GetChecksumsCmd -c netarkivet -s $CONFDIR > $listing
+java -cp "/nas/lib/*" org.bitrepository.commandline.GetChecksumsCmd -c $COLLECTION -s $CONFDIR > $listing
+echo Found files:
+cat $listing
 
  while read line; do
     if [[ $line != *":"* ]]; then
      arr=($line)
      for PILLAR in $PILLARS; do
-       java -cp "/nas/lib/*" org.bitrepository.commandline.DeleteFileCmd -c netarkivet -s $CONFDIR -p $PILLAR -i ${arr[2]} -C ${arr[0]}
+       echo "Deleting ${arr[2]} from $PILLAR"
+       java -cp "/nas/lib/*" org.bitrepository.commandline.DeleteFileCmd -c $COLLECTION -s $CONFDIR -p $PILLAR -i ${arr[2]} -C ${arr[0]}
      done
     fi
  done < $listing
