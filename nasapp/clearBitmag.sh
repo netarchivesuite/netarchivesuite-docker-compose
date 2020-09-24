78fb55e80191de764ca0e470815621add8681750
csrster <csr@statsbiblioteket.dk>
2020-09-24 13:41:57 +0200

    Added logging config

--- nasapp/clearBitmag.sh
+++ nasapp/clearBitmag.sh
@@ -6,11 +6,12 @@ COLLECTION=netarkivet
 BASEDIR=$(dirname $0)
 CONFDIR="/nas/nasclientconfig"
 PILLARS="kb-pillar"
+LOGGING="-Dlogback.configurationFile=$CONFDIR/logback.xml"
 
 listing=$(mktemp)
 trap "rm -f $listing" 0 2 3 5
 
-java -cp "/nas/lib/*" org.bitrepository.commandline.GetChecksumsCmd -c $COLLECTION -s $CONFDIR > $listing
+java -cp "/nas/lib/*" $LOGGING org.bitrepository.commandline.GetChecksumsCmd -c $COLLECTION -s $CONFDIR > $listing
 echo Found files:
 cat $listing
 
@@ -19,7 +20,7 @@ cat $listing
      arr=($line)
      for PILLAR in $PILLARS; do
        echo "Deleting ${arr[2]} from $PILLAR"
-       java -cp "/nas/lib/*" org.bitrepository.commandline.DeleteFileCmd -c $COLLECTION -s $CONFDIR -p $PILLAR -i ${arr[2]} -C ${arr[0]}
+       java -cp "/nas/lib/*" $LOGGING org.bitrepository.commandline.DeleteFileCmd -c $COLLECTION -s $CONFDIR -p $PILLAR -i ${arr[2]} -C ${arr[0]}
      done
     fi
  done < $listing
@@ -28,7 +29,7 @@ cat $listing
 
 for file in /nas/testdata/*; do
     echo "Uploading $file"
-    java -cp "/nas/lib/*" org.bitrepository.commandline.PutFileCmd -c $COLLECTION -s $CONFDIR -f $file
+    java -cp "/nas/lib/*" $LOGGING org.bitrepository.commandline.PutFileCmd -c $COLLECTION -s $CONFDIR -f $file
 done
 
 echo Finished uploading test data
