2a29e40c1b6069bd36d8f34d14484908ed74b4bd
csrster <csr@kb.dk>
2020-11-26 14:25:10 +0100

    Fixed up special case where regex starts with a "/"

--- fileresolver/scripts/find.sh
+++ fileresolver/scripts/find.sh
@@ -4,6 +4,10 @@ dir=$2
 if [ -z $dir ]; then
   locate --regex --basename -d /db/db.db $1
 else
-  fullregex='/'$dir'/'$regex
+  if [[ ${str:0:1} == "/" ]] ; then
+    fullregex='/'$dir'/'$regex
+  else
+    fullregex='/'$dir$regex
+  fi
   locate --regex -d /db/db.db $fullregex
 fi
